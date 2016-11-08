d4d95dab777e1a9107bea79cd8fd29770e53131e
Cecil Lee <cmjlee3@gmail.com>
2016-11-08 15:44:26 -0500

    ??

--- models/movie.js
+++ models/movie.js
@@ -9,15 +9,44 @@ const limit = 10;
 const offset = 0;
 
 function getAllMovies(req, res, next) {
+  db.any('SELECT * FROM movies;')
+  .then((movies) => {
+    res.movies = movies;
+    next();
+  })
+  .catch(error => next(error));
 // implement get all movies
 }
 
 function getMovie(req, res, next) {
 // implement get single movie
+  let ID = Number.parseInt(req.params.ID);
+  db.one(`SELECT *
+        FROM movies
+        WHERE id = $1;`, [ID])
+  .then((movies) => {
+    res.movies = movies;
+    next();
+  })
+   .catch(error => next(error));
 }
 
 function updateMovie(req, res, next) {
 // implement update
+  console.log(req.body)
+  cecil = Number.parseInt(req.params.id);
+  db.none(`UPDATE movies
+    SET title=$2
+    WHERE id=$1;`,
+    [ cecil, req.body.title ])
+  // .then(r=> r.json())
+  .then(data=>{
+    console.log(data)
+    next()
+  })
+  .catch(function (err) {
+    return next(err);
+  });
 }
 
 function deletemovie(req, res, next) {
--- routes/api/movies.js
+++ routes/api/movies.js
@@ -1,13 +1,34 @@
 const express = require('express');
 const router = express.Router();
-const { getAllMovies, getMovie, getAllMoviesWithRatings } = require('../../models/movie');
+const { getAllMovies, getMovie, getAllMoviesWithRatings, updateMovie } = require('../../models/movie');
 
+// ALL ROUTES HERE START WITH '/api/movies'
+
+const sendJSONresp = (req, res) => res.json(res.movies);
 // handle all the routes
+router.route('/')
+  .get(getAllMovies, sendJSONresp);
 
 // get all movies
-
+// router.get('/', getAllMovies, (req, res) => {
+//   res.json(res.movies)
+// });
 // Get movies withrating BONUS
 
-// Get single movie
+
+// // Get single movie
+// router.get('/:ID', getMovie, (req, res) => {
+//   res.json(res.movies)
+// });
+
+router.route('/:id')
+  .get(getMovie, sendJSONresp)
+  .put(updateMovie, (req,res)=>{
+    res.status(200)
+    .json({
+      status: 'su',
+      message: 'Update Movie'
+    })})
+  .delete();
 
 module.exports = router;
